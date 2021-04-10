951daf80f319420b1e5dc1280e01624ee7c3666e
ElizavetaGradoboeva <79892867+ElizavetaGradoboeva@users.noreply.github.com>
2021-04-10 17:31:29 +0600

    Update lab.5.11.cpp

--- lab.5.11.cpp
+++ lab.5.11.cpp
@@ -5,130 +5,125 @@
 */
 
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
-int main() {
+int main()
+{
+    srand(time(NULL));
+    setlocale(LC_ALL, "rus");
 
-	setlocale(LC_ALL, "rus");
+    cout << "\n     Программа вычисляет произведение двух матриц. \n\n ";
 
-	cout << " Программа вычисляет произведение двух матриц. \n\n ";
+    int line_size_A, size_A_B, column_size_B;
+   
+    cout << " Введите колличество строк в матрице А : ";
 
-	int line_size_A,  column_size_B, size_A_B;
-	int i, j;
+    while (!(cin >> line_size_A))
+    {
+        cin.clear();
+        while (cin.get() != '\n');
+        cout << "Ошибка! Введена буква " << endl;
+        cout << "Введите колличество строк в матрице A : ";
+    }
 
-	cout << " Введите колличество строк в матрице А : ";
+    cout << "" Введите количество столбцов в матрице А и количество строк в матрице В : "";
 
-	while (!(cin >> line_size_A))
-	{
-		cin.clear();
-		while (cin.get() != '\n');
-	  cout << " Ошибка! Введена буква " << endl;
-		cout << " Введите колличество строк в матрице A : ";
-	}
+    while (!(cin >> size_A_B))
+    {
+        cin.clear();
+        while (cin.get() != '\n');
+        cout << "Ошибка! Введена буква " << endl;
+        cout << "Введите количество столбцов в матрице А и количество строк в матрице В : ";
+    }
 
-	cout << " Введите количество столбцов в матрице А и количество строк в матрице В :  "; 
+    cout << " Введите количество столбцов в матрице А и количество строк в матрице В : ";
 
-	while (!(cin >> size_A_B))
-	{
-		cin.clear();
-		while (cin.get() != '\n');
-		cout << " Ошибка! Введена буква " << endl;
-		cout << " Введите количество столбцов в матрице А и количество строк в матрице В : ";
-		}
+    while (!(cin >> column_size_B))
+    {
+        cin.clear();
+        while (cin.get() != '\n');
+        cout << "Ошибка! Введена буква " << endl;
+        cout << "Введите колличество столбцов в матрице B : ";
+    }
 
-	cout << " Введите колличество столбцов в матрице В : ";
+    int** A, ** B, ** C;
 
-	while (!(cin >> column_size_B))
-	{
-		cin.clear();
-		while (cin.get() != '\n');
-		cout << " Ошибка! Введена буква " << endl;
-		cout << " Введите колличество столбцов в матрице B : ";
-	}
+    A = new int* [line_size_A];
+    
+    for (int i = 0; i < line_size_A; i++)
+    {
+        A[i] = new int[size_A_B];
+       
+        for (int j = 0; j < size_A_B; j++)
+        {
+            A[i][j] = rand() % 10;
+           
+        }
+    }
+  
+    B = new int* [size_A_B];
+   
+    for (int i = 0; i < size_A_B; i++)
+    {
+        B[i] = new int[column_size_B];
+     
+        for (int j = 0; j < column_size_B; j++)
+        {
+            B[i][j] = rand() % 10;
+        }
+    }
+  
+    C = new int* [line_size_A];
+    for (int i = 0; i < line_size_A; i++)
+    {
+        C[i] = new int[column_size_B];
+        for (int j = 0; j < column_size_B; j++)
+        {
+            C[i][j] = 0;
+        }
+    }
 
-	int** aArr = new int* [line_size_A];
-
-	for (i = 0; i < line_size_A; i++)
-		
-		aArr[i] = new int[size_A_B];
-
-	for (i = 0; i < line_size_A; i++)
-	{
-		for (j = 0; j < size_A_B; j++)
-		{
-			aArr[i][j] = rand() % 100;
-		}
-	}
-
-
-	cout << " Матрица А: " << endl;
-
-	for (i = 0; i < line_size_A; i++)
-	{
-		for (j = 0; j < size_A_B; j++){
-			cout << aArr[i][j] << '\t';
-		}
-		cout << endl;
-	}
-	
-	int** bArr = new int* [size_A_B];
-
-	for (i = 0; i < size_A_B; i++)
-
-			bArr[i] = new int[column_size_B];
-
-	for (i = 0; i < size_A_B; i++)
-	{
-		for (j = 0; j < column_size_B; j++)
-		{
-			bArr[i][j] = rand() % 100;
-		}
-	}
-
-	cout << " Матрица В: " << endl;
-
-	for (i = 0; i < size_A_B; i++)
-	{
-		for (j = 0; j < column_size_B; j++) {
-			cout << bArr[i][j] << '\t';
-		}
-		cout << endl;
-	}
+    for (int i = 0; i < line_size_A; i++)
+        for (int j = 0; j < column_size_B; j++)
+            for (int v = 0; v < size_A_B; v++)
+                C[i][j] += A[i][v] * B[v][j];
 
 
-	int** cArr = new int* [i];
+    cout << "\n Матрица А :" << endl;
+    for (int i = 0; i < line_size_A; i++)
+    {
+        for (int j = 0; j < size_A_B; j++)
+            cout << A [i][j] << " ";
+        cout << endl;
+    }
 
-	for (i = 0; i < line_size_A; i++)
+    cout << "\n Матрица В : "<< endl;
+    for (int i = 0; i < size_A_B; i++)
+    {
+        for (int j = 0; j < column_size_B; j++)
+            cout << B[i][j] << " ";
+        cout << endl;
+    }
 
-		cArr[i] = new int[j];
+    cout << "\n Матрица С = (А*В)" << endl;
+    for (int i = 0; i < line_size_A; i++)
+    {
+        for (int j = 0; j < column_size_B; j++)
+            cout << C[i][j] << " ";
+        cout << endl;
+    }
 
-	for (i = 0; i < line_size_A; i++)
+    for (int i = 0; i < line_size_A; i++)
+    {
+        delete[]A[i];
+        delete[]B[i];
+        delete[]C[i];
+    }
+    delete[]A;
+    delete[]B;
+    delete[]C;
 
-		for (j = 0; j < column_size_B; j++)
-		{
-			cArr[i][j] = 0;
-
-			for (int k = 0; k <= column_size_B + 1; k++) {
-
-				cArr[i][j] += (aArr[i][k] * bArr[k][j]);
-			}
-		}
-
-	cout << " Матрица С: " << endl;
-
-	for (i = 0; i < line_size_A; i++)
-	{
-		for (j = 0; j < column_size_B; j++) {
-			cout << cArr[i][j] << '\t';
-		}
-		cout << endl;
-	}
-	delete[] aArr;
-	delete[] bArr;
-	delete[] cArr;
-
-	system ("pause");
-	return 0;
-
+    return 0;
 }
